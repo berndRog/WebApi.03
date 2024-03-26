9390d67f8e25623335bdf826981d3c02ccec6122
rogallab <b-u.rogalla@ostfalia.de>
2024-03-26 18:42:18 +0100

    Postman added

--- WebApi.Core/Core/DomainModel/Entities/Owner.cs
+++ WebApi.Core/Core/DomainModel/Entities/Owner.cs
@@ -8,7 +8,7 @@ public class Owner: IEntity {
    #region properties
    public Guid Id { get; init; } = Guid.NewGuid();
    public string   Name     { get; set; } = string.Empty;
-   public DateTime Birthdate{ get; set; } = DateTime.UtcNow;
+   public DateTime Birthdate{ get; init; } = DateTime.UtcNow;
    public string   Email    { get; set; } = string.Empty;
    
    // Navigation property

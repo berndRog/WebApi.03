5645ba22308efd696963ef9454a20eed3ba719d8
rogallab <b-u.rogalla@ostfalia.de>
2024-05-18 16:52:33 +0200

    AsIban all 4 Digits a space is included

--- WebApi.Core/Core/Misc/Utils.cs
+++ WebApi.Core/Core/Misc/Utils.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 namespace WebApi.Core.Misc; 
 public static class Utils {
    
@@ -21,5 +22,8 @@ public static class Utils {
 
    
    public static string As8(this Guid guid) => guid.ToString()[..8];
+   
+   public static string AsIban(this string s) => Regex.Replace(s, ".{4}", "$0 ").Trim();
+
 
 }
